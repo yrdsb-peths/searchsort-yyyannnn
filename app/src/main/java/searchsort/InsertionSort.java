d1d3c83af4cf4828b1f32ed5a5c8076ab62c1e26
yyyannnn <349168872@gapps.yrdsb.ca>
2024-12-02 22:00:09 -0500

    finished insertionSort

--- app/src/main/java/searchsort/InsertionSort.java
+++ app/src/main/java/searchsort/InsertionSort.java
@@ -5,6 +5,16 @@ public class InsertionSort {
      * Sort the array arr using insertion sort algorithm.
      */
     public static void sort(int[] arr) {
+        int n = arr.length;
 
+        for(int i = 1; i < n; i++) {
+            for(int j = i; j > 0; j--) {
+                if(arr[j-1] > arr[j]) {
+                    Util.exch(arr, j, j-1);
+                }else {
+                    break;
+                }
+            }
+        }
     }
 }
