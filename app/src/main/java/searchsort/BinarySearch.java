7aef02bff4364cf831caaf69753cec7ecb3ed47e
yyyannnn <349168872@gapps.yrdsb.ca>
2024-12-02 21:49:02 -0500

    Finished binarySearch

--- app/src/main/java/searchsort/BinarySearch.java
+++ app/src/main/java/searchsort/BinarySearch.java
@@ -7,7 +7,19 @@ public class BinarySearch extends Search {
      */
     @Override
     public int find(int[] arr, int target) {
-        return 0;
+        int lo = 0;
+        int hi = arr.length -1;
+        while (lo <= hi) {
+            int mid = (lo + hi)/2;
+            if(target < arr[mid]) {
+                hi = mid - 1;
+            } else if (target > arr[mid]) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
     }
 
     /**
@@ -16,7 +28,25 @@ public class BinarySearch extends Search {
      */
     @Override
     public int recursiveFind(int[] arr, int target) {
-        return 0;
+        return recursiveFindHelper(arr, target, 0, arr.length-1);
+    }
+
+    private int recursiveFindHelper(int[] arr, int target, int lo, int hi) {
+        if(lo > hi) {
+            return -1;
+        }
+
+        int mid = (lo + hi)/2;
+
+        if(target == arr[mid]) {
+            return mid;
+        }
+
+        if(target > mid) {
+            return recursiveFindHelper(arr, target, mid+1, hi);
+        } 
+
+        return recursiveFindHelper(arr, target, lo, mid-1);
     }
 
 }
