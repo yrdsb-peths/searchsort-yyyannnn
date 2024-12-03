2d1b33c53cdaa4b4a223f7cccdef1040da96b255
yyyannnn <349168872@gapps.yrdsb.ca>
2024-12-02 21:21:06 -0500

    Finished LinearSearch

--- app/src/main/java/searchsort/LinearSearch.java
+++ app/src/main/java/searchsort/LinearSearch.java
@@ -6,8 +6,11 @@ public class LinearSearch {
      * Return -1 if target is not found in arr.
      */
     public static int find(int[] arr, int target) {
-        
-        
-        return 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
